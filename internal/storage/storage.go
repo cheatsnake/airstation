24b16b8ecdd4fcb244de8f7d44a4bb717df23c4f
cheatsnake <72691412+cheatsnake@users.noreply.github.com>
2025-01-05 22:27:28 +0300

    feat: add storage module

---
+++ internal/storage/storage.go
@@ -0,0 +1,14 @@
+package storage
+
+import "github.com/cheatsnake/airstation/internal/entities/track"
+
+type TrackStore interface {
+	AddTrack(name, path string, duration float64, bitrate int) (*track.Track, error)
+
+	RemoveTracks(IDs []string) error
+
+	EditTrack(track track.Track) (*track.Track, error)
+
+	FindTrack(ID string) (*track.Track, error)
+	FindTracks(IDs []string) ([]*track.Track, error)
+}
