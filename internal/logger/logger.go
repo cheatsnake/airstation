6e8428747f91e54a57664378a9d3d09ecc0381dd
cheatsnake <72691412+cheatsnake@users.noreply.github.com>
2025-02-01 18:40:35 +0300

    feat: update logger module

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -2,9 +2,14 @@ package logger
 
 import (
 	"log/slog"
+	"os"
 )
 
 func New() *slog.Logger {
-	logger := slog.New(slog.Default().Handler())
+	handler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	})
+
+	logger := slog.New(handler)
 	return logger
 }
