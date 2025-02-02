36d3b854a82edf8c4b9aa861abb1fc13b3e92159
cheatsnake <72691412+cheatsnake@users.noreply.github.com>
2025-02-02 16:03:10 +0300

    feat: update track module

--- internal/track/queue.go
+++ internal/track/queue.go
@@ -5,20 +5,36 @@ import (
 	"slices"
 )
 
+// Queue represents a collection of audio tracks.
 type Queue struct {
-	Tracks []Track
+	Tracks []Track `json:"tracks"` // A slice containing the tracks currently in the queue.
 }
 
+// NewQueue creates and returns a new Queue instance initialized with a given list of tracks.
+//
+// Parameters:
+//   - tracks: A slice of Track instances to initialize the queue.
+//
+// Returns:
+//   - A pointer to a newly created Queue instance.
 func NewQueue(tracks []Track) *Queue {
 	return &Queue{
 		Tracks: tracks,
 	}
 }
 
+// Add appends new tracks to the end of the queue.
+//
+// Parameters:
+//   - tracks: A slice of Track instances to add to the queue.
 func (q *Queue) Add(tracks []Track) {
 	q.Tracks = append(q.Tracks, tracks...)
 }
 
+// Remove removes tracks from the queue based on their track IDs.
+//
+// Parameters:
+//   - trackIDs: A slice of track IDs to remove from the queue.
 func (q *Queue) Remove(trackIDs []string) {
 	filtered := make([]Track, 0, len(q.Tracks)-len(trackIDs))
 
@@ -31,6 +47,13 @@ func (q *Queue) Remove(trackIDs []string) {
 	q.Tracks = filtered
 }
 
+// Reorder reorders the tracks in the queue based on the provided list of track IDs.
+//
+// Parameters:
+//   - trackIDs: A slice of track IDs representing the desired order of tracks in the queue.
+//
+// Returns:
+//   - An error if any track ID does not exist in the queue.
 func (q *Queue) Reorder(trackIDs []string) error {
 	ordered := make([]Track, 0, len(trackIDs))
 
@@ -47,6 +70,13 @@ func (q *Queue) Reorder(trackIDs []string) error {
 	return nil
 }
 
+// FindTrack finds and returns the track with the given track ID, or nil if not found.
+//
+// Parameters:
+//   - trackID: The ID of the track to search for.
+//
+// Returns:
+//   - A pointer to the found Track, or nil if no matching track is found.
 func (q *Queue) FindTrack(trackID string) *Track {
 	for _, t := range q.Tracks {
 		if t.ID == trackID {
@@ -57,6 +87,10 @@ func (q *Queue) FindTrack(trackID string) *Track {
 	return nil
 }
 
+// CurrentTrack returns the first track in the queue, or nil if the queue is empty.
+//
+// Returns:
+//   - A pointer to the first track in the queue, or nil if the queue is empty.
 func (q *Queue) CurrentTrack() *Track {
 	if len(q.Tracks) == 0 {
 		return nil
@@ -65,6 +99,10 @@ func (q *Queue) CurrentTrack() *Track {
 	return &q.Tracks[0]
 }
 
+// NextTrack returns the second track in the queue, or the first if there is only one track, or nil if the queue is empty.
+//
+// Returns:
+//   - A pointer to the second track, or the first track if there's only one, or nil if the queue is empty.
 func (q *Queue) NextTrack() *Track {
 	if len(q.Tracks) == 0 {
 		return nil
@@ -77,6 +115,7 @@ func (q *Queue) NextTrack() *Track {
 	return &q.Tracks[1]
 }
 
+// Spin shifts the queue by moving the first track to the end of the queue, effectively rotating the queue by one position.
 func (q *Queue) Spin() {
 	if len(q.Tracks) < 2 {
 		return
--- internal/track/track.go
+++ internal/track/track.go
@@ -1,21 +1,34 @@
+// Package track defines the structure and functions for managing audio track entities.
 package track
 
 import "github.com/cheatsnake/airstation/internal/tools/ulid"
 
+// Track represents an audio track with its associated metadata.
 type Track struct {
-	ID       string
-	Name     string
-	Path     string
-	Duration float64 // Seconds
-	Bitrate  int     // Kbps
+	ID       string  `json:"id"`       // A unique identifier for the track, typically generated using ULID.
+	Name     string  `json:"name"`     // The name of the audio track.
+	Path     string  `json:"path"`     // The file path of the audio track.
+	Duration float64 `json:"duration"` // The duration of the audio track in seconds.
+	BitRate  int     `json:"bitRate"`  // The bit rate of the audio track in kilobits per second (kbps).
 }
 
-func New(name, path string, duration float64, bitrate int) *Track {
+// New creates and returns a new Track instance with the provided name, path, duration, and bit rate.
+// It also generates a unique ID for the track.
+//
+// Parameters:
+//   - name: The name of the track.
+//   - path: The file path of the track.
+//   - duration: The duration of the track in seconds.
+//   - bitRate: The bit rate of the track in kilobits per second (kbps).
+//
+// Returns:
+//   - A pointer to the newly created Track instance.
+func New(name, path string, duration float64, bitRate int) *Track {
 	return &Track{
 		ID:       ulid.New(),
 		Name:     name,
 		Path:     path,
 		Duration: duration,
-		Bitrate:  bitrate,
+		BitRate:  bitRate,
 	}
 }
