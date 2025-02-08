dade6b7fe25379e3b15105117f0bbe8859ba423e
cheatsnake <72691412+cheatsnake@users.noreply.github.com>
2025-02-08 14:24:14 +0300

    feat: update config module

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,11 +5,14 @@ import (
 	"path/filepath"
 )
 
+const minSecretKeyLength = 10
+
 type Config struct {
 	TracksDir string
 	TmpDir    string
 	WebDir    string
 	HTTPPort  string
+	SecretKey string
 }
 
 func Load() *Config {
@@ -18,6 +21,7 @@ func Load() *Config {
 		TmpDir:    getEnv("AIRSTATION_TMP_DIR", filepath.Join("static", "tmp")),
 		WebDir:    getEnv("AIRSTATION_WEB_DIR", filepath.Join("web")),
 		HTTPPort:  getEnv("AIRSTATION_HTTP_PORT", "8080"),
+		SecretKey: getSecretKey("AIRSTATION_SECRET_KEY"),
 	}
 }
 
@@ -26,4 +30,20 @@ func getEnv(key, defaultValue string) string {
 		return value
 	}
 	return defaultValue
+}
+
+func getSecretKey(key string) string {
+	secretKey := os.Getenv(key)
+
+	if secretKey == "" {
+		println(key + " environment variable is not set")
+		os.Exit(1)
+	}
+
+	if len(secretKey) < minSecretKeyLength {
+		println(key + " is too short")
+		os.Exit(1)
+	}
+
+	return secretKey
 }
