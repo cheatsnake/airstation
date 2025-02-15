a1dec9ee08725d68493a512a7a7f14134f5b307b
cheatsnake <72691412+cheatsnake@users.noreply.github.com>
2025-02-15 20:41:52 +0300

    feat: update storage module

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -7,16 +7,16 @@ type Storage interface {
 }
 
 type TrackStore interface {
-	GetTracks(page, limit int) ([]*track.Track, int, error)
+	Tracks(page, limit int) ([]*track.Track, int, error)
 	FindTrack(ID string) (*track.Track, error)
 	FindTracks(IDs []string) ([]*track.Track, error)
 	AddTrack(name, path string, duration float64, bitRate int) (*track.Track, error)
-	RemoveTracks(IDs []string) error
+	DeleteTracks(IDs []string) error
 	EditTrack(track *track.Track) (*track.Track, error)
 
-	GetQueue() ([]*track.Track, error)
+	Queue() ([]*track.Track, error)
 	AddToQueue(tracks []*track.Track) error
-	DeleteFromQueue(trackIDs []string) error
+	RemoveFromQueue(trackIDs []string) error
 	ReorderQueue(trackIDs []string) error
 	SpinQueue() error
 	CurrentAndNextTrack() (*track.Track, *track.Track, error)
