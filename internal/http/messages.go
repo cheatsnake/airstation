c6b9e10b3b2f8137c29420c75cd9aa4e04950a98
cheatsnake <72691412+cheatsnake@users.noreply.github.com>
2025-02-19 20:01:04 +0300

    feat: add helpers for json responses

---
+++ internal/http/messages.go
@@ -0,0 +1,41 @@
+package http
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Message struct {
+	Message string `json:"message"`
+}
+
+func jsonResponse(w http.ResponseWriter, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		http.Error(w, "JSON encoding failed", http.StatusInternalServerError)
+	}
+}
+
+func jsonMessage(w http.ResponseWriter, code int, body string) {
+	msg := Message{Message: body}
+
+	w.WriteHeader(code)
+	jsonResponse(w, msg)
+}
+
+func jsonOK(w http.ResponseWriter, body string) {
+	jsonMessage(w, http.StatusOK, body)
+}
+
+func jsonBadRequest(w http.ResponseWriter, body string) {
+	jsonMessage(w, http.StatusBadRequest, body)
+}
+
+func jsonUnauthorized(w http.ResponseWriter, body string) {
+	jsonMessage(w, http.StatusUnauthorized, body)
+}
+
+func jsonInternalError(w http.ResponseWriter, body string) {
+	jsonMessage(w, http.StatusInternalServerError, body)
+}
